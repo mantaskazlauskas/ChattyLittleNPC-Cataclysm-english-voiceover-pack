9ff58215586da77b990c5b81af2c4759c7607acf
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:39:03 +0300

    Init commit part 1

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_Cataclysm_voiceovers
+local ChattyLittleNpc_Cataclysm_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Cataclysm_voiceovers")
+
+ChattyLittleNpc_Cataclysm_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
